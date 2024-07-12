fdb3ac79685abc5f1da04c975efb84998bac4e20
ToriKnix <toriknixmusic@gmail.com>
2024-07-12 09:41:47 -0600

    initial commit

---
+++ src/components/HeroSection.js
@@ -0,0 +1,20 @@
+export default function HeroSection(){
+    return(
+        <div className="section">
+            <div className="col">
+                <h1 className="title">
+                    What Are We About?
+                </h1>
+                <p className="info">
+                    FoodiesHub is a place where you can please your soul
+                    and tummy with delicious food recipes of all cuisine.
+                    And our service is absolutely free. So start exploring now.
+                    <button className="btn">explore now</button>
+                </p>
+            </div>
+            <div className="col">
+                col 2
+            </div>
+        </div>
+    )
+}
